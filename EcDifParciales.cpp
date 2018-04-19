e1d5aea422d6abe9ef3c733e690d6d0e968cb8bb
Mauricio Neira <m.neira10@uniandes.edu.co>
2018-04-18 22:43:40 -0500

    funciona

--- EcDifParciales.cpp
+++ EcDifParciales.cpp
@@ -6,58 +6,89 @@
 #include <ctime>
 using namespace std;
 
+double deltax = 0.1;
+double deltat = 0.01;
+double c = 1.0;
+int n = (2.0 / deltax) + 1;
+
 //declaracion metodos
-double u_inicial(double x);
-
-
+double u_inicial(double *u, double x0, int n, double delax);
+double uj_1(double *u, int i);
+void imprimirU(double *u);
+void actualizarU(double *u);
 
 //main
-int main() {
-  double deltax=0.1;
-  double deltat=0.01;
-  double T=0.5;
-  double xi=0;
-  double tj=0;
-  int n=(max_x-min_x)/deltax+1;
-  u=new double [n]
+int main()
+{
+
+  double T = 0.5;
+  double xi = 0;
+  
+  
+  double u[n];
   //se calcula pata t+deltat
+  
+  u_inicial(u, xi, n, deltax);
+  imprimirU(u);
 
-  u_inicial(u*,xi,n,deltax);
-  for (int i=0;i<=n;i++){  
-    double tj-=(deltat*(u[i+1]-u[i]))/deltax;
-    cout<<xi<<" "<< tj<<endl;
-  }
-
-    return 0;
+  actualizarU(u);
+  actualizarU(u);
+  actualizarU(u);
+  actualizarU(u);
+  actualizarU(u);
+  actualizarU(u);
+  actualizarU(u);
+  
+  
+  imprimirU(u);
+  
+  return 0;
 }
 
-double u_inicial(double u*,double x0, int n;double deltax){
-  u[0]=x0;
-  for(int i=0;i<=n;i++){
-    double x=x0;
-    if(x>=0 && x<=0.75){
-      return u[i]=1;
+double u_inicial(double *u, double x0, int n, double deltax)
+{
+  double x = x0;
+  for (int i = 0; i < n; i++)
+  {
+    if (x >= 0 && x <= 0.75)
+    {
+      u[i] = 1;
+      // cout<<x << " "<<u[i] <<endl;
+    } 
+    else
+    {
+      u[i] = 0;
+      // cout<<x << " "<<u[i]<<endl;
     }
-    else{
-      return u[i]=0;
-    }
-    x+=deltax;
+    x += deltax;
   }
 }
 
+double uj_1(double *u, int i)
+{
+  if (i == n - 1)
+  {
+    return u[i];
+  }
+  else
+  {
+    return u[i] + c*(deltat/deltax)*(u[i+1]-u[i]);
+  }
+}
+
+void imprimirU(double *u){
+  for (int i = 0; i < n; i++)
+  {
+    u[i] = uj_1(u,i);
+    cout << u[i] << " " ;
+  }
+  cout<<endl;
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-//metodos
+void actualizarU(double *u){
+  for (int i = 0; i < n; i++)
+  {
+    u[i] = uj_1(u,i);
+  }
+}
